7f1bd317f78fd098f80960f20fca9ae8b47555b8
BinaryHippo <162812672+BinaryHippo@users.noreply.github.com>
2024-06-09 05:55:48 +0200

    Add files via upload
    Add scatter plot matrix, adjusted interface

--- server.R
+++ server.R
@@ -3,9 +3,32 @@ library(brochure)
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+library(MASS)
+library(GGally)
 
 #Server code of the app
 server <- function(input, output, session) {
+  
+  #Function to generate panel
+  panel.hist <- function(x, ...)
+  {
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(usr[1:2], 0, 1.5) )
+    h <- hist(x, plot = FALSE)
+    breaks <- h$breaks; nB <- length(breaks)
+    y <- h$counts; y <- y/max(y)
+    rect(breaks[-nB], 0, breaks[-1], y, col = "cyan", ...)
+  }
+  panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+  {
+    usr <- par("usr"); on.exit(par(usr))
+    par(usr = c(0, 1, 0, 1))
+    r <- abs(cor(x, y))
+    txt <- format(c(r, 0.123456789), digits = digits)[1]
+    txt <- paste0(prefix, txt)
+    if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+    text(0.5, 0.5, txt, cex = cex.cor * r)
+  }
   
   #Error function in case of non-numeric values
   displayError <- function()
@@ -21,7 +44,25 @@ server <- function(input, output, session) {
            "state.x77" = as.data.frame(state.x77) %>% 
              rename("Life_Exp" = 4, "HS_Grad" = 6),
            "LakeHuron" = data.frame(row.names = c(1875:1972), Depth = LakeHuron),
-           "Titanic" = as.data.frame(Titanic))
+           "Titanic" = as.data.frame(Titanic),
+           "PimaIndians" = as.data.frame(Pima.tr),
+           "PipettingTraining" = data.frame(
+             before = c(1.36, 1.37, 1.29, 1.22, 1.38, 1.31, 1.40, 1.39, 1.30, 1.37), 
+             after = c(1.29, 1.25, 1.20, 1.26, 1.25, 1.23, 1.26, 1.31, 1.24, 1.31)),
+           "BacterialCulture" = data.frame(
+             grow = c(0.146842, 0.156757, 0.091255, 0.063720, 0.148471, -0.045436, 0.150407,
+                      0.077905, 0.077267, 0.026454, 0.090700, 0.245384, 0.129650, 0.141617,
+                      0.039957, 0.165351, 0.029091, 0.073473, 0.189657, 0.123897)),
+             "Salary" = data.frame(
+               company1 = c(4218.874, 2323.970, 4104.761, 3172.519, 3058.287, 2386.729,
+                            4405.709, 2665.709, 5326.124, 2993.015, 5152.121, 3164.876,
+                            2703.269, 3837.005, 2927.137, 2847.995, 3087.938, 3063.339,
+                            4697.341, 5602.379, 2992.996, 5052.060, 4095.423, 1668.059, 6268.097),
+               company2 = c(1888.252, 2429.395, 2062.037, 1932.138, 1788.335, 2119.263,
+                            2185.819, 2173.098, 2391.626, 1576.546, 1871.540, 2405.640, 
+                            2470.771, 1879.237, 2181.048, 2272.962, 2174.767, 1729.053,
+                            1119.993, 2325.788, 2112.610, 2847.006, 1124.272, 5320.000, 4785.000))
+    )
   })
   
   # Returns selected variable
@@ -29,7 +70,7 @@ server <- function(input, output, session) {
     input$variable
   })
   
-  # UI for variable selection
+  # UI for variable selection single
   output$variableSelect <- renderUI({
     data <- selectedData()
     if (is.data.frame(data)) {
@@ -49,11 +90,15 @@ server <- function(input, output, session) {
     if (!is.null(var) && is.numeric(data[[var]])) {
       ggplot(data, aes_string(x = var)) +
         geom_histogram(bins = bins, fill = "blue", color = "black") +
-        labs(title = paste("Histogram \n[", var, "]"), x = var, y = "Frequency")
+        labs(title = paste("Histogram \n[", var, "]"), x = var, y = "Frequency") + 
+        theme(axis.text = element_text(size = 15), 
+              axis.ticks.length=unit(.25, "cm"),
+              axis.title=element_text(size=15,face="bold"),
+              plot.title = element_text(face="bold", size=20))
     } else {
       displayError()
     }
-  })
+  }, height = 700, width = 700)
   
   # Output the selected dataset as a box plot using ggplot2
   output$box <- renderPlot({
@@ -65,7 +110,11 @@ server <- function(input, output, session) {
           pivot_longer(cols = everything(), names_to = "variable", values_to = "value")
         ggplot(data_long, aes(x = variable, y = value)) +
           geom_boxplot(fill = "blue", color = "black") +
-          labs(title = "Box Plot \n[all variables]", x = "Variable", y = "Value")
+          labs(title = "Box Plot \n[all variables]", x = "Variable", y = "Value") + 
+          theme(axis.text = element_text(size = 15), 
+                axis.ticks.length=unit(.25, "cm"),
+                axis.title=element_text(size=15,face="bold"),
+                plot.title = element_text(face="bold", size=20))
       } else {
         displayError()
       }
@@ -73,12 +122,16 @@ server <- function(input, output, session) {
       if (!is.null(var) && is.numeric(data[[var]])) {
         ggplot(data, aes_string(y = var)) +
           geom_boxplot(fill = "blue", color = "black") +
-          labs(title = paste("Box Plot \n[", var, "]"), y = var)
+          labs(title = paste("Box Plot \n[", var, "]"), y = var) + 
+          theme(axis.text = element_text(size = 15), 
+                axis.ticks.length=unit(.25, "cm"),
+                axis.title=element_text(size=15,face="bold"),
+                plot.title = element_text(face="bold", size=20))
       } else {
         displayError()
       }
     }
-  })
+  }, height = 700, width = 700)
   
   # Output the selected dataset as a Q-Q plot using ggplot2
   output$qq <- renderPlot({
@@ -88,11 +141,15 @@ server <- function(input, output, session) {
       ggplot(data, aes_string(sample = var)) +
         geom_qq() +
         geom_qq_line(color = "red") +
-        labs(title = paste("Q-Q Plot \n[", var, "]"))
+        labs(title = paste("Q-Q Plot \n[", var, "]")) + 
+        theme(axis.text = element_text(size = 15), 
+              axis.ticks.length=unit(.25, "cm"),
+              axis.title=element_text(size=15,face="bold"),
+              plot.title = element_text(face="bold", size=20))
     } else {
       displayError()
     }
-  })
+  }, height = 700, width = 700)
   
   # Output the selected dataset as an ECDF plot using ggplot2
   output$ecdf <- renderPlot({
@@ -101,9 +158,24 @@ server <- function(input, output, session) {
     if (!is.null(var) && is.numeric(data[[var]])) {
       ggplot(data, aes_string(x = var)) +
         stat_ecdf(geom = "step", color = "blue") +
-        labs(title = paste("ECDF Plot \n[", var, "]"), x = var, y = "ECDF")
+        labs(title = paste("ECDF Plot \n[", var, "]"), x = var, y = "ECDF") + 
+        theme(axis.text = element_text(size = 15), 
+              axis.ticks.length=unit(.25, "cm"),
+              axis.title=element_text(size=15,face="bold"),
+              plot.title = element_text(face="bold", size=20))
     } else {
       displayError()
     }
-  })
+  }, height = 700, width = 700)
+  
+  # Output the selected dataset as a Scatter Plot Matrix
+  output$scatter_matrix <- renderPlot({
+    data <- selectedData()
+    if (all(sapply(data, is.numeric))) {
+      pairs(data, lower.panel = panel.smooth, upper.panel = panel.cor, 
+            diag.panel = panel.hist,las=1) 
+    } else {
+      displayError()
+    }
+  }, height = 700, width = 700)
 }
